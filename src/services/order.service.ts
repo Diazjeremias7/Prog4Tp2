26cd39ad29624ac38c9b424c87b944477d9ecc25
Bernapai <juanemeunier@gmail.com>
2025-10-05 19:34:00 -0300

    test(VERDE): implementar precio para pizza S

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -2,6 +2,7 @@ import { PizzaSize } from "../types/types";
 
 export class OrderService {
     calculatePrice(size: PizzaSize, toppings: string[]): number {
+        if (size === 'S') return 10;
         return 0;
     }
 }
